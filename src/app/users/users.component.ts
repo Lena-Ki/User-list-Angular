7d9b68beb8be367a4cc2dd6dca337c515d7f60cc
Helen Kim <lenahomerus@gmail.com>
2020-09-02 22:29:23 +0300

    interface User added

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
+interface User {
+  id: number
+  name: string
+  date: any
+}
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
+
 export class UsersComponent implements OnInit {
+
+  @Input() users: User[] = [
+    {id: 1, name: 'Helen', date: new Date()},
+    {id: 2, name: 'Kim', date: new Date()},
+    {id: 3, name: 'Olga', date: new Date()},
+  ]
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.users)
   }
 
 }
